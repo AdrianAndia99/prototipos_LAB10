37a03aa4aee6593dfbb9d10d2c6ac1a2d638b12e
AdrianAndia99 <151102699+AdrianAndia99@users.noreply.github.com>
2025-05-20 11:16:46 -0500

    confe

---
+++ Assets/Scripts/AllienProblem/Allien.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace AllienProblem
+{
+    public class Allien
+    {
+        public List<Vector3> eyePositions;
+        public List<Vector3> legPositions;
+        public List<Vector3> armPositions;
+
+        public Allien()
+        {
+
+        }
+
+        public void SetEyePosition(List<Vector3> positions)
+        {
+            eyePositions = positions;
+        }
+
+        public void SetLegPosition(List<Vector3> positions)
+        {
+            legPositions = positions;
+        }
+
+        public void SetArmPosition(List<Vector3> positions)
+        {
+            armPositions = positions;
+        }
+    }
+}
---
+++ Assets/Scripts/AllienProblem/BaseProblem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace AllienProblem
+{
+    public abstract class BaseProblem : MonoBehaviour
+    {
+        [Header("Allien Problem")]
+        [SerializeField] protected int NumberAlliens = 50;
+        [SerializeField] protected int NumberBodyParts = 50;
+
+        protected List<Allien> allAlliens = new List<Allien>();
+
+        protected abstract void Start();
+
+        protected List<Vector3> GetBodyPartPositions()
+        {
+            List<Vector3> bodyPartPositions = new List<Vector3>();
+
+            for (int i = 0; i < NumberBodyParts; i++)
+            {
+                bodyPartPositions.Add(new Vector3());
+            }
+
+            return bodyPartPositions;
+        }
+    }
+}
